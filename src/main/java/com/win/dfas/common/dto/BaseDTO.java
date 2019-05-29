7860d13fdf4cc5131d651a36d5054c716d69db83
hechengcheng <hechengcheng@ysstech.com>
2019-05-29 11:58:14 +0800

    [add-新增功能] 功能描述: 新增基础对象 影响范围: StoryID :

---
+++ src/main/java/com/win/dfas/common/dto/BaseDTO.java
@@ -0,0 +1,44 @@
+/****************************************************
+ * 创建人：     @author hechengcheng    
+ * 创建时间: 2019年5月29日/上午11:32:17
+ * 项目名称：  dfas-common-util
+ * 文件名称: BaseDTO.java
+ * 文件描述: @Description: TODO(用一句话描述该文件做什么)
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2019
+ * 
+ ********************************************************/
+
+package com.win.dfas.common.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**   
+ * 包名称： com.win.dfas.common.dto 
+ * 类名称：BaseDTO 
+ * 类描述：服务之间的数据转换基础类
+ * 创建人：@author hechengcheng 
+ * 创建时间：2019年5月29日/上午11:32:17
+ *     
+ */
+@Getter
+@Setter
+@ToString
+public class BaseDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer userId;
+	
+	private String ip;
+	
+	private String mac;
+	
+	private String hostName;
+
+}
---
+++ src/main/java/com/win/dfas/common/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+/****************************************************
+ * 创建人：     @author hechengcheng    
+ * 创建时间: 2019年5月29日/上午10:59:19
+ * 项目名称：  dfas-common-util
+ * 文件名称: BaseEntity.java
+ * 文件描述: @Description: 
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2019
+ * 
+ ********************************************************/
+
+package com.win.dfas.common.entity;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**   
+ * 包名称： com.win.dfas.common.entity 
+ * 类名称：BaseEntity 
+ * 类描述：数据库对应的实体基础类
+ * 创建人：@author hechengcheng 
+ * 创建时间：2019年5月29日/上午10:59:19
+ *     
+ */
+@Getter
+@Setter
+@ToString
+public class BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private Long createUserId;
+	
+	private String createTime;
+	
+	private Long updateUserId;
+	
+	private String updateTime;
+
+}
