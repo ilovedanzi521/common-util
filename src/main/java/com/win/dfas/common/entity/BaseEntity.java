2a01fa0aeac6518008f5d334c0a0f7a7559dd8c6
zhoujinyin <zhoujinyin@ysstech.com>
2019-06-10 11:47:50 +0800

    [modify-问题修复] 问题原因:用户信息无法获取 解决方法:加入user 请求时自动映射 影响范围: BugID   :

--- src/main/java/com/win/dfas/common/entity/BaseEntity.java
+++ src/main/java/com/win/dfas/common/entity/BaseEntity.java
@@ -15,7 +15,6 @@ package com.win.dfas.common.entity;
 import java.io.Serializable;
 
 import com.win.dfas.common.util.PrimaryKeyUtil;
-import com.win.dfas.common.util.UserUtil;
 
 import cn.hutool.core.date.DateUtil;
 import lombok.Data;
@@ -34,7 +33,7 @@ public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
-	
+
 	private Integer deleteFlag;
 
 	private String createUserId;
@@ -44,46 +43,48 @@ public class BaseEntity implements Serializable {
 	private String updateUserId;
 
 	private String updateTime;
-	
+
+	private BaseUserInfo user;
+
 	/**
-	 * 
+	 *
 	 * @Title: preInsert
-	 * @Description: 预插入实体公用参数处理  
-	 * @return: void   
+	 * @Description: 预插入实体公用参数处理
+	 * @return: void
 	 * @throws
-	 * @author: hechengcheng 
+	 * @author: hechengcheng
 	 * @Date:  2019年6月3日/下午7:01:51
 	 */
 	public void preInsert() {
         this.id = PrimaryKeyUtil.generateId();
         this.deleteFlag = 0;
-        this.createUserId = UserUtil.getUserId();
+        this.createUserId = user.getUserId();
         this.createTime = DateUtil.now();
     }
-	
+
 	/**
-	 * 
+	 *
 	 * @Title: preUpdate
 	 * @Description: 预修改实体公用参数处理
-	 * @return   
-	 * @return: void   
+	 * @return
+	 * @return: void
 	 * @throws
-	 * @author: hechengcheng 
+	 * @author: hechengcheng
 	 * @Date:  2019年6月3日/下午6:53:18
 	 */
     public void preUpdate() {
-        this.updateUserId = UserUtil.getUserId();
+        this.updateUserId = user.getUserId();
         this.updateTime = DateUtil.now();
     }
-    
+
     /**
-     * 
+     *
      * @Title: preDelete
      * @Description: 预删除实体公用参数处理
-     * @return   
-     * @return: void   
+     * @return
+     * @return: void
      * @throws
-     * @author: hechengcheng 
+     * @author: hechengcheng
      * @Date:  2019年6月3日/下午6:55:19
      */
     public void preDelete() {
