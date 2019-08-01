84bd106ca34ad36a3e60117a585b97091aa366ea
hechengcheng <hechengcheng@ysstech.com>
2019-08-01 15:06:17 +0800

    [modify-问题修复] 问题原因:新增数据字典常量 解决方法: 影响范围: BugID   :

--- src/main/java/com/win/dfas/common/constant/DicConstants.java
+++ src/main/java/com/win/dfas/common/constant/DicConstants.java
@@ -3,7 +3,7 @@
  * 创建时间: 2019年7月27日/下午3:41:43
  * 项目名称：  dfas-common-util
  * 文件名称: DicConstants.java
- * 文件描述: @Description: TODO(用一句话描述该文件做什么)
+ * 文件描述: @Description: 数据字典常量类
  *
  * All rights Reserved, Designed By 投资交易团队
  * @Copyright:2016-2019
@@ -11,18 +11,419 @@
  ********************************************************/
 package com.win.dfas.common.constant;
 
-/**   
+/**
+ * 
  * 包名称： com.win.dfas.common.constant 
  * 类名称：DicConstants 
- * 类描述：TODO
+ * 类描述：数据字典常量
  * 创建人：@author hechengcheng 
- * 创建时间：2019年7月27日/下午3:41:43
- *     
+ * 创建时间：2019年8月1日/下午3:03:21
+ *
  */
 public final class DicConstants {
+	
+	/**
+	 * 公司类别
+	 */
+	public static String PDIC_1000101 = "1000101";
+
+	/**
+	 * 交易市场
+	 */
+	public static String PDIC_1000102 = "1000102";
+
+	/**
+	 * 交易平台
+	 */
+	public static String PDIC_1000103 = "1000103";
+
+	/**
+	 * 日志类型
+	 */
+	public static String PDIC_1000104 = "1000104";
+
+	/**
+	 * 企业性质
+	 */
+	public static String PDIC_1000105 = "1000105";
+
+	/**
+	 * 内部评级
+	 */
+	public static String PDIC_1000106 = "1000106";
+
+	/**
+	 * 外部评级
+	 */
+	public static String PDIC_1000107 = "1000107";
+
+	/**
+	 * 时区
+	 */
+	public static String PDIC_1000108 = "1000108";
+
+	/**
+	 * 申报方向
+	 */
+	public static String PDIC_1000109 = "1000109";
+
+	/**
+	 * 资金方向
+	 */
+	public static String PDIC_1000110 = "1000110";
+
+	/**
+	 * 证券方向
+	 */
+	public static String PDIC_1000111 = "1000111";
+
+	/**
+	 * 影响证券
+	 */
+	public static String PDIC_1000112 = "1000112";
+
+	/**
+	 * 关联变动方向
+	 */
+	public static String PDIC_1000113 = "1000113";
+
+	/**
+	 * 关联影响证券
+	 */
+	public static String PDIC_1000114 = "1000114";
+
+	/**
+	 * 市场类型
+	 */
+	public static String PDIC_1000115 = "1000115";
+
+	/**
+	 * 证券大类
+	 */
+	public static String PDIC_1000116 = "1000116";
+
+	/**
+	 * 证券类别单位
+	 */
+	public static String PDIC_1000117 = "1000117";
+
+	/**
+	 * 申报单位
+	 */
+	public static String PDIC_1000118 = "1000118";
+
+	/**
+	 * 资金冻结方式
+	 */
+	public static String PDIC_1000119 = "1000119";
+
+	/**
+	 * 买卖最小数量控制方式
+	 */
+	public static String PDIC_1000120 = "1000120";
+
+	/**
+	 * 市场交易日
+	 */
+	public static String PDIC_1000121 = "1000121";
+
+	/**
+	 * 国家地区
+	 */
+	public static String PDIC_1000122 = "1000122";
+
+	/**
+	 * 投资标志
+	 */
+	public static String PDIC_1000123 = "1000123";
+
+	/**
+	 * 网上网下
+	 */
+	public static String PDIC_1000125 = "1000125";
+
+	/**
+	 * 基金类型
+	 */
+	public static String PDIC_1000201 = "1000201";
+
+	/**
+	 * 投资分类
+	 */
+	public static String PDIC_1000202 = "1000202";
+
+	/**
+	 * 收费方式
+	 */
+	public static String PDIC_1000203 = "1000203";
+
+	/**
+	 * 申赎状态
+	 */
+	public static String PDIC_1000204 = "1000204";
+
+	/**
+	 * 债券付息类型
+	 */
+	public static String PDIC_1000205 = "1000205";
+
+	/**
+	 * 债券利率类型
+	 */
+	public static String PDIC_1000206 = "1000206";
+
+	/**
+	 * 债券基准类型
+	 */
+	public static String PDIC_1000207 = "1000207";
+
+	/**
+	 * 债券计息基准
+	 */
+	public static String PDIC_1000208 = "1000208";
+
+	/**
+	 * 债券报价方式
+	 */
+	public static String PDIC_1000209 = "1000209";
+
+	/**
+	 * 债券托管机构
+	 */
+	public static String PDIC_1000210 = "1000210";
+
+	/**
+	 * 是否可赎回
+	 */
+	public static String PDIC_1000211 = "1000211";
+
+	/**
+	 * 是否可回售
+	 */
+	public static String PDIC_1000212 = "1000212";
+
+	/**
+	 * 本金偿还类型
+	 */
+	public static String PDIC_1000213 = "1000213";
+
+	/**
+	 * 分期偿还类型
+	 */
+	public static String PDIC_1000214 = "1000214";
+
+	/**
+	 * 发行方式
+	 */
+	public static String PDIC_1000216 = "1000216";
+
+	/**
+	 * 证券状态
+	 */
+	public static String PDIC_1000217 = "1000217";
+
+	/**
+	 * 杂项分类
+	 */
+	public static String PDIC_1000218 = "1000218";
+
+	/**
+	 * 银行账户类型
+	 */
+	public static String PDIC_1000220 = "1000220";
+
+	/**
+	 * 资金账号类型
+	 */
+	public static String PDIC_1000222 = "1000222";
+
+	/**
+	 * 资金账户状态
+	 */
+	public static String PDIC_1000223 = "1000223";
+
+	/**
+	 * 券商类型
+	 */
+	public static String PDIC_1000224 = "1000224";
+
+	/**
+	 * 组合类型
+	 */
+	public static String PDIC_1000225 = "1000225";
+
+	/**
+	 * 登记公司
+	 */
+	public static String PDIC_1000226 = "1000226";
+
+	/**
+	 * 产品类型
+	 */
+	public static String PDIC_1000227 = "1000227";
+
+	/**
+	 * 产品状态
+	 */
+	public static String PDIC_1000228 = "1000228";
+
+	/**
+	 * 资产币种
+	 */
+	public static String PDIC_1000229 = "1000229";
+
+	/**
+	 * 募集币种
+	 */
+	public static String PDIC_1000230 = "1000230";
+
+	/**
+	 * 股东属性
+	 */
+	public static String PDIC_1000265 = "1000265";
+
+	/**
+	 * 股东登记状态
+	 */
+	public static String PDIC_1000266 = "1000266";
+
+	/**
+	 * 当天是否交易
+	 */
+	public static String PDIC_1000267 = "1000267";
+
+	/**
+	 * 托管机构
+	 */
+	public static String PDIC_1000268 = "1000268";
+
+	/**
+	 * 可转股债券类型
+	 */
+	public static String PDIC_1000269 = "1000269";
+
+	/**
+	 * 托管账户类型
+	 */
+	public static String PDIC_1000270 = "1000270";
+
+	/**
+	 * 结算方式
+	 */
+	public static String PDIC_1000271 = "1000271";
+
+	/**
+	 * 对手方评级
+	 */
+	public static String PDIC_1000272 = "1000272";
+
+	/**
+	 * 清算速度
+	 */
+	public static String PDIC_1000274 = "1000274";
 
 	/**
 	 * 投资标志
 	 */
 	public static String PDIC_1000275 = "1000275";
+
+	/**
+	 * 对手方托管行账户停用标志
+	 */
+	public static String PDIC_1000276 = "1000276";
+
+	/**
+	 * 对手方资金账户停用标志
+	 */
+	public static String PDIC_1000277 = "1000277";
+
+	/**
+	 * 指令状态
+	 */
+	public static String PDIC_1000278 = "1000278";
+
+	/**
+	 * 指令状态(过滤条件)
+	 */
+	public static String PDIC_1000279 = "1000279";
+
+	/**
+	 * 指令类型
+	 */
+	public static String PDIC_1000280 = "1000280";
+
+	/**
+	 * 标准券折算率数据来源
+	 */
+	public static String PDIC_1000281 = "1000281";
+
+	/**
+	 * 机构角色
+	 */
+	public static String PDIC_1000282 = "1000282";
+
+	/**
+	 * 网下网上
+	 */
+	public static String PDIC_1000283 = "1000283";
+
+	/**
+	 * 增发次数
+	 */
+	public static String PDIC_1000284 = "1000284";
+
+	/**
+	 * 招标标的
+	 */
+	public static String PDIC_1000285 = "1000285";
+
+	/**
+	 * 承销方式
+	 */
+	public static String PDIC_1000286 = "1000286";
+
+	/**
+	 * 数据来源
+	 */
+	public static String PDIC_1000287 = "1000287";
+
+	/**
+	 * 机构信用评级类型
+	 */
+	public static String PDIC_1000288 = "1000288";
+
+	/**
+	 * 评级展望
+	 */
+	public static String PDIC_1000289 = "1000289";
+
+	/**
+	 * 债券评级方式
+	 */
+	public static String PDIC_1000290 = "1000290";
+
+	/**
+	 * 变动方向
+	 */
+	public static String PDIC_1000291 = "1000291";
+
+	/**
+	 * 机构评级来源
+	 */
+	public static String PDIC_1000292 = "1000292";
+
+	/**
+	 * 债券评级类型
+	 */
+	public static String PDIC_1000293 = "1000293";
+
+	/**
+	 * 募集方式
+	 */
+	public static String PDIC_1000294 = "1000294";
+
+	/**
+	 * 指令业务分类
+	 */
+	public static String PDIC_1000295 = "1000295";
 }
