7c691f96504608813cb19bd02227e9073c4b077c
jinxiaolong <jinxiaolong@ysstech.com>
2019-10-10 17:20:19 +0800

    [modify-问题修复] 问题原因: ObjectUtils添加对象转Map的方法 解决方法: 影响范围:

--- src/main/java/com/win/dfas/common/util/ObjectUtils.java
+++ src/main/java/com/win/dfas/common/util/ObjectUtils.java
@@ -24,6 +24,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -38,6 +40,7 @@ import com.win.dfas.common.enumeration.FormatEnum;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.core.util.ObjectUtil;
+import org.springframework.cglib.beans.BeanMap;
 
 /**   
  * 包名称： com.win.dfas.common.util 
@@ -424,6 +427,90 @@ public class ObjectUtils {
 		}
 		
 		return annoFieldMap;
+	}
+
+	/**
+	 * 功能描述 <br>
+	 * 将bean转为Map
+	 *
+	 * @author jinxiaolong
+	 * @date 2019/10/10 11:34
+	 * @param bean
+	 * @return java.util.Map<java.lang.String,java.lang.Object>
+	 */
+	public static <T> Map<String, Object> beanToMap(T bean) {
+		Map<String, Object> map = Maps.newHashMap();
+		if (bean != null) {
+			BeanMap beanMap = BeanMap.create(bean);
+			for (Object key : beanMap.keySet()) {
+				map.put(key+"", beanMap.get(key));
+			}
+		}
+		return map;
+	}
+
+	/**
+	 * 功能描述 <br>
+	 * 将Map转为Bean
+	 *
+	 * @author jinxiaolong
+	 * @date 2019/10/10 11:35
+	 * @param map
+	 * @param bean
+	 * @return T
+	 */
+	public static <T> T mapToBean(Map<String, Object> map,T bean) {
+		BeanMap beanMap = BeanMap.create(bean);
+		beanMap.putAll(map);
+		return bean;
+	}
+
+	/**
+	 * 功能描述 <br>
+	 * 将List<T>转换为List<Map<String, Object>>
+	 *
+	 * @author jinxiaolong
+	 * @date 2019/10/10 11:35
+	 * @param objList
+	 * @return java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
+	 */
+	public static <T> List<Map<String, Object>> objectsToMaps(List<T> objList) {
+		List<Map<String, Object>> list = Lists.newArrayList();
+		if (objList != null && objList.size() > 0) {
+			Map<String, Object> map = null;
+			T bean = null;
+			for (int i = 0,size = objList.size(); i < size; i++) {
+				bean = objList.get(i);
+				map = beanToMap(bean);
+				list.add(map);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * 功能描述 <br>
+	 * 将List<Map<String,Object>>转换为List<T>
+	 *
+	 * @author jinxiaolong
+	 * @date 2019/10/10 11:36
+	 * @param maps
+	 * @param clazz
+	 * @return java.util.List<T>
+	 */
+	public static <T> List<T> mapsToObjects(List<Map<String, Object>> maps,Class<T> clazz) throws InstantiationException, IllegalAccessException {
+		List<T> list = Lists.newArrayList();
+		if (maps != null && maps.size() > 0) {
+			Map<String, Object> map = null;
+			T bean = null;
+			for (int i = 0,size = maps.size(); i < size; i++) {
+				map = maps.get(i);
+				bean = clazz.newInstance();
+				mapToBean(map, bean);
+				list.add(bean);
+			}
+		}
+		return list;
 	}
 	
 }
